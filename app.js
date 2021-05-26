4dfe701ae09ad3eb886edbe92674a1667c7e7788
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-26 02:07:41 +0000

    add squares to grid

--- app.js
+++ app.js
@@ -12,4 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const rotateButton = document.querySelector('#rotate');
     const turnDisplay = document.querySelector('#whose-go');
     const infoDisplay = document.querySelector('#info');
+
+    const userSquares=[];
+    const computerSquares=[];
+
+    const width =10;
+    //create boards
+    function createBoard(grid,squares,width){
+        for (let i =0;i<width*width;i++){
+            const square = document.createElement('div');
+            square.dataset.id=i;
+            grid.appendChild(square);
+            squares.push(square);
+        }
+    }
+
+    createBoard(userGrid,userSquares,width);
+    createBoard(computerGrid,computerSquares,width);
 })
